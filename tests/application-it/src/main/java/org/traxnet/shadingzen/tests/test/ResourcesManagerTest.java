e8e060471ae78df63a90ffa62733e8957b7396fd
Oscar Blasco <traxnet@gmail.com>
2012-12-18 21:08:44 +0100

    Added missing files for tests

---
+++ tests/application-it/src/main/java/org/traxnet/shadingzen/tests/test/ResourcesManagerTest.java
@@ -0,0 +1,52 @@
+package org.traxnet.shadingzen.tests.test;
+
+import android.test.ActivityInstrumentationTestCase2;
+import org.traxnet.shadingzen.tests.*;
+import org.traxnet.shadingzen.core.*;
+
+public class ResourcesManagerTest extends ActivityInstrumentationTestCase2<HelloAndroidActivity> {
+
+    public ResourcesManagerTest() {
+        super(HelloAndroidActivity.class);
+    }
+
+    public void testManagerInit() {
+        HelloAndroidActivity activity = getActivity();
+        assertNotNull(activity);
+
+        ResourcesManager res_manager = new ResourcesManager();
+        assertNotNull(res_manager);
+
+    }
+
+    public void testManagerSetExpansionPack() {
+        ResourcesManager res_manager = createResourcesManager();
+    }
+
+    private ResourcesManager createResourcesManager() {
+        ResourcesManager res_manager = new ResourcesManager();
+        assertNotNull(res_manager);
+
+        try{
+            res_manager.setExpansionPack("/mnt/sdcard/shadingzen/resources.zip");
+            res_manager.setContext(getActivity().getApplicationContext());
+        } catch (Exception ex){
+            assertTrue("Exception trown:" + ex.getMessage(), false);
+        }
+        return res_manager;
+    }
+
+    public void testBitmap2DDecodingFromExmpansionPack(){
+        ResourcesManager res_manager = createResourcesManager();
+
+        BitmapTexture.Parameters params = new BitmapTexture.Parameters();
+        CompressedResource res = res_manager.factoryCompressed(BitmapTexture.class,  null, "test", "", params);
+        assertNull(res);
+
+        res = res_manager.factoryCompressed(BitmapTexture.class,  null, "test", "resources/textures/exodus_01.jpg", params);
+        assertNotNull(res);
+    }
+
+
+}
+
